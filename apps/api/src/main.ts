8ddaa52a7b708a4a84d4a297ab1c810819ac3398
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-10-18 23:45:27 -0700

    Creates routes dir

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,6 +15,7 @@ app.get('/api', (req, res) => {
 });
 
 const port = process.env.PORT || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
