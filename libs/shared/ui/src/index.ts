edea4253565d15b18219e0db19a4e9a5ee637f37
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-10-21 11:25:11 -0700

    Rename Header component file name

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,2 +1,2 @@
-export * from './lib/header/header';
+export * from './lib/header';
 export * from './lib/shared-ui';
--- libs/shared/ui/src/lib/header/index.spec.tsx
+++ libs/shared/ui/src/lib/header/index.spec.tsx
@@ -1,11 +1,12 @@
 import { render } from '@testing-library/react';
 
-import { Header } from './header';
+import { Header } from '.';
 
 describe('Header', () => {
   it('should render successfully', () => {
+    // expect(true).toBe(true);
+    const { baseElement } = render(<Header />);
     expect(true).toBe(true);
-    // const { baseElement } = render(<Header />);
     // expect(baseElement).toBeTruthy();
   });
 });
