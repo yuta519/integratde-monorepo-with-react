074cedcc30f2db9e5580582406c3543186d972fd
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-10-21 14:22:14 -0700

    Changes import order

--- libs/shared/ui/src/lib/header/index.spec.tsx
+++ libs/shared/ui/src/lib/header/index.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { Header } from '.';
-import { ChakraProvider } from '@chakra-ui/react';
 
 describe('Header', () => {
   it('should render successfully', () => {
