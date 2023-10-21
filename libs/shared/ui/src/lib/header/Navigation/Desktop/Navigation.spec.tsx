44800ec1d7fb84b3bec4861d325ba17bc0bc1f70
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-10-21 15:29:59 -0700

    Adds tests to practice testing-library

--- libs/shared/ui/src/lib/header/Navigation/Desktop/Navigation.spec.tsx
+++ libs/shared/ui/src/lib/header/Navigation/Desktop/Navigation.spec.tsx
@@ -1,27 +1,26 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { render, screen } from '@testing-library/react';
 
 import { Navigation } from '.';
 
 describe('Navigation', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <ChakraProvider>
-        <Navigation />
-      </ChakraProvider>
-    );
+    const { baseElement } = render(<Navigation />);
     expect(baseElement).toBeTruthy();
   });
 
   it('should have proper link paths in the website', () => {
-    const { getByText } = render(
-      <ChakraProvider>
-        <Navigation />
-      </ChakraProvider>
-    );
+    const { getByText } = render(<Navigation />);
 
     expect(getByText('Inspiration').closest('a')).toHaveAttribute('href', '#');
     expect(getByText('Find Work').closest('a')).toHaveAttribute('href', '#');
+  });
+
+  it('should have proper link paths in the website', () => {
+    render(<Navigation />);
+
+    expect(screen.getByText('Inspiration').closest('a')).toHaveTextContent(
+      'Inspiration'
+    );
   });
 });
