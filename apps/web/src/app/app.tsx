ebad2286c14ee4140eaf61e1a7d1256fc6ef2834
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-10-31 14:00:28 -0700

    Erase unnecessary parts

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -3,28 +3,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { Header } from '@integratde-monorepo-with-react/shared-ui';
 import { adapters } from '@integratde-monorepo-with-react/adapters';
+import Articles from './views/articles';
 import NxWelcome from './nx-welcome';
 
-function Home() {
-  return <h1>Home</h1>;
-}
-
-const Proudcts = () => {
-  return <>aaaaaaaaaaaaaaaaaa</>;
-};
-
-export function App() {
+export default function App() {
   adapters();
   return (
     <ChakraProvider>
       <Header />
       <Routes>
         <Route path="/" element={<NxWelcome title="web" />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/products" element={<Proudcts />}></Route>
+        <Route path="/articles" element={<Articles />}></Route>
       </Routes>
     </ChakraProvider>
   );
 }
-
-export default App;
